c068fc3ac1c535130ec565b4abd739d11d08e6db
amanwhskan <156500603+amanwhskan@users.noreply.github.com>
2025-08-13 23:18:17 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,31 @@
+const navs = document.querySelectorAll('.nav-list li');
+const cube = document.querySelector('.box');
+const section = document.querySelectorAll('.section');
 const resumelists = document.querySelectorAll('.resume-list');
 const resumeboxs = document.querySelectorAll('.resume-box');
+const projectList = document.querySelectorAll('.project-list');
+const projectboxs = document.querySelectorAll('.project-box');
+navs.forEach((nav, idx)=>{
+    nav.addEventListener('click',()=>{
+        document.querySelector('.nav-list li.active').classList.remove('active');
+        nav.classList.add('active');
+
+        cube.style.transform = `rotateY(${idx * -90}deg)`;
+         document.querySelector('.section.active').classList.remove('active');
+        section[idx].classList.add('active');
+
+        const array = Array.from(section);
+        const arrSec= array.slice(1,-1);
+        arrSec.forEach(arrSec =>{ 
+            if (arrSec.classList.contains('active')) {
+                section[4].classList.add('action-contact');
+            }
+        });
+        if (section[0].classList.contains('active')) {
+            section[4].classList.remove('action-contact');
+        }
+    }); 
+});
 resumelists.forEach((lists,idx)=>{
     lists.addEventListener('click',()=>{
         document.querySelector('.resume-list.active').classList.remove('active');
@@ -9,3 +35,15 @@ resumelists.forEach((lists,idx)=>{
         resumeboxs[idx].classList.add('active');
     });
 });
+projectList.forEach((list,idx)=>{
+    list.addEventListener('click',()=>{
+        document.querySelector('.project-list.active').classList.remove('active');
+        list.classList.add('active');
+
+       document.querySelector('.project-box.active').classList.remove('active');
+        projectboxs[idx].classList.add('active');
+    });
+});
+setTimeout(() => {
+    section[4].classList.remove('active');
+}, 1500);
